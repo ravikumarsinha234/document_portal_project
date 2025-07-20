82121edebcbdbf6fdb58371a48f4038425eb7e8d
Ravi <ravikumarsinha234@gmail.com>
2025-07-20 15:34:27 +0530

    updating the basic syntax and requirements

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='document_portal_genai_project',
+    author='Ravi Kumar Sinha',
+    version='0.1.0',
+    packages=find_packages(),
+)
